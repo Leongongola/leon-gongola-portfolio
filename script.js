39fdbdaeeddd6adf5f5a4023a429420ac010c793
leongongola <leongongola@gmail.com>
2022-07-25 20:38:47 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// function for the hambuger menu to rotate
+function myFunction() {
+    var x = document.querySelector(".menu-container");
+    x.classList.toggle("change");
+  }
+// function for drop down menu to display block if its display none or vise versa
+function dropmenu(){
+    var x = document.getElementById('menu-dropdown');
+    if (x.style.display === "block"){
+        x.style.display = "none";
+    }else{
+        x.style.display = "block";
+    }
+}
+// adding eventlistener for the click to show the drop down menu
+const navmenu = document.getElementById("menu-dropdown");
+navmenu.addEventListener("click", () => {
+    navmenu.classList.toggle("active");
+});
+
+
